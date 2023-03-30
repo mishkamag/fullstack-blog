194dae9334e97931989089728423cad76294edf8
mishkamag <m93.maglaperidze@gmail.com>
2023-03-30 19:10:44 +0400

    start of frontend

---
+++ client/src/components/topbar/TopBar.jsx
@@ -0,0 +1,13 @@
+import "./topbar.css";
+
+const TopBar = () => {
+  return (
+    <div className="top">
+      <div className="topLeft">1</div>
+      <div className="topCenter">2</div>
+      <div className="topRight">3</div>
+    </div>
+  );
+};
+
+export default TopBar;
