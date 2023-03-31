6ee916262526614a92e0cb4741d15dd8b818bb98
mishkamag <m93.maglaperidze@gmail.com>
2023-04-01 00:01:05 +0400

    fixed fail

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -12,15 +12,6 @@ const Header = () => {
         alt=""
         className="headerImg"
       />
-      <div className="postInfo">
-        <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
-        </div>
-        <span className="postTitle">Lorem ipsum dolor sit amet</span>
-        <hr />
-        <span className="postDate">1 hour ago</span>
-      </div>
     </div>
   );
 };
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -7,6 +7,15 @@ const Post = () => {
         src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
         alt=""
       />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Music</span>
+          <span className="postCat">Life</span>
+        </div>
+        <span className="postTitle">Lorem ipsum dolor sit amet</span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
     </div>
   );
 };
