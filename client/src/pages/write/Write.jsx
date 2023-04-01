3b0cbb5c6065b680d0c567f3ae92948a5ffcdd03
mishkamag <m93.maglaperidze@gmail.com>
2023-04-01 18:16:42 +0400

    work with write post page

---
+++ client/src/pages/write/Write.jsx
@@ -0,0 +1,33 @@
+import "./write.css";
+import { AiOutlineFolderAdd } from "react-icons/ai";
+
+const Write = () => {
+  return (
+    <div className="write">
+      <form className="writeForm">
+        <div className="writeFromGroup">
+          <label htmlFor="fileInput">
+            <AiOutlineFolderAdd size={24} />
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFromGroup">
+          <textarea
+            placeholder="Tell your story"
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <div className="writeSubmit">Publish</div>
+      </form>
+    </div>
+  );
+};
+
+export default Write;
