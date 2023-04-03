cb92c7426f8c91fda5fa6cb3e5818413b7c05589
mishkamag <m93.maglaperidze@gmail.com>
2023-04-04 00:21:52 +0400

    amoust ff frontend

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -4,8 +4,10 @@ import { FaInstagramSquare } from "react-icons/fa";
 import { BsPinterest } from "react-icons/bs";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const TopBar = () => {
+  const user = true;
   return (
     <div className="top">
       <div className="topLeft">
@@ -16,19 +18,57 @@ const TopBar = () => {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li>HOME</li>
-          <li>ABOUT</li>
-          <li>CONTACT</li>
-          <li>WRITE</li>
-          <li>LOGOUT</li>
+          <li>
+            <Link to="/" className="link">
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="link">
+              ABOUT
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="link">
+              CONTACT
+            </Link>
+          </li>
+          <li>
+            <Link to="/write" className="link">
+              WRITE
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="link">
+              {user && "LOGOUT"}
+            </Link>
+          </li>
         </ul>
       </div>
       <div className="topRight">
-        <img
-          src="https://scontent.ftbs6-2.fna.fbcdn.net/v/t1.6435-9/65296906_1039287772940954_5380062896111747072_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=e3f864&_nc_ohc=GbwA_yS-AywAX98OLEF&_nc_ht=scontent.ftbs6-2.fna&oh=00_AfCMLLInQC07Pb3qpis6Lt0_u_uy1znII9s6LcuDrgHt7A&oe=644D137F"
-          alt=""
-          className="topImg"
-        />
+        {user ? (
+          <img
+            src="https://scontent.ftbs6-2.fna.fbcdn.net/v/t1.6435-9/65296906_1039287772940954_5380062896111747072_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=e3f864&_nc_ohc=GbwA_yS-AywAX98OLEF&_nc_ht=scontent.ftbs6-2.fna&oh=00_AfCMLLInQC07Pb3qpis6Lt0_u_uy1znII9s6LcuDrgHt7A&oe=644D137F"
+            alt=""
+            className="topImg"
+          />
+        ) : (
+          <ul className="topList">
+            <li>
+              {" "}
+              <Link to="/login" className="link">
+                LOGIN
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/register" className="link">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+
         <AiOutlineSearch className="topSearchIcon" size={29} />
       </div>
     </div>
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./register.css";
 
 const Register = () => {
@@ -13,7 +14,12 @@ const Register = () => {
         <input type="password" placeholder="Enter your password..." />
         <button className="registerButton">Register</button>
       </form>
-      <button className="registerLoginButton">Login</button>
+      <button className="registerLoginButton">
+        {" "}
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </button>
     </div>
   );
 };
