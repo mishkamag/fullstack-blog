13cd8e5e2474fee86d01a15c1b403076004321b8
mishkamag <m93.maglaperidze@gmail.com>
2023-04-03 23:41:02 +0400

    working with router

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,22 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <>
+    <Router>
       <TopBar />
-      {/* <Home /> */}
-      {/* <Single /> */}
-      {/* <Write /> */}
-      {/* <Settings /> */}
-      {/* <Login /> */}
-      <Register />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
