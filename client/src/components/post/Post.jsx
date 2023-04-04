f6480bf2184d0962a48132470e4625e9cfc85e20
mishkamag <m93.maglaperidze@gmail.com>
2023-04-05 01:29:18 +0400

    working with posts

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,33 +1,35 @@
+import { Link } from "react-router-dom";
 import "./post.css";
 
-const Post = () => {
+const Post = ({ post }) => {
+  console.log(post);
   return (
     <div className="post">
-      <img
-        src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-        alt=""
-      />
+      {post.photo ? (
+        <img src={post.photo} alt="" />
+      ) : (
+        <img
+          src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
+          alt=""
+        />
+      )}
+
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">Music</span>
-          <span className="postCat">Life</span>
+          {post.categories.map((category) => (
+            <span className="postCat">{category.name}</span>
+          ))}
         </div>
-        <span className="postTitle">Lorem ipsum dolor sit amet</span>
+        <Link to={`/post/${post.id}`} className="link">
+          <span className="postTitle ">{post.title}</span>
+        </Link>
+
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga libero est
-        maxime qui inventore iure ducimus placeat soluta reiciendis. Beatae
-        voluptates officia molestiae quae hic, veniam modi. Provident, quidem
-        tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-        libero est maxime qui inventore iure ducimus placeat soluta reiciendis.
-        Beatae voluptates officia molestiae quae hic, veniam modi. Provident,
-        quidem tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Fuga libero est maxime qui inventore iure ducimus placeat soluta
-        reiciendis. Beatae voluptates officia molestiae quae hic, veniam modi.
-        Provident, quidem tempore!
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 };
