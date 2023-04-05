79e718967c8df3f368128992081263f70d1742a8
mishkamag <m93.maglaperidze@gmail.com>
2023-04-05 20:41:20 +0400

    working register connect to db

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,20 +1,54 @@
 import { Link } from "react-router-dom";
 import "./register.css";
+import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("http://localhost:5000/api/auth/register", {
+      username,
+      email,
+      password,
+    });
+    console.log(res);
+  };
+
   return (
     <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
-        <input type="text" placeholder="Enter your username..." />
+        <input
+          type="text"
+          placeholder="Enter your username..."
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
         <label>Email</label>
-        <input type="text" placeholder="Enter your email..." />
+        <input
+          type="text"
+          placeholder="Enter your email..."
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
         <label>Password</label>
-        <input type="password" placeholder="Enter your password..." />
+        <input
+          type="password"
+          placeholder="Enter your password..."
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
         <button className="registerButton">Register</button>
       </form>
-      <button className="registerLoginButton">
+      <button className="registerLoginButton" type="submit">
         {" "}
         <Link className="link" to="/login">
           Login
